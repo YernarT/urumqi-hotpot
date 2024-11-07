1ef9655918816d3f6f73536128645fe5d47dd3b0
YernarT <toktaryernar@gmail.com>
2024-11-07 19:17:19 +0500

    feat: :sparkles: home page

---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="home-page">
+    <MainSection />
+  </main>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: "IndexPage" });
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.home-page {
+  
+}
+</style>
